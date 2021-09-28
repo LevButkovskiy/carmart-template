c99353e9c0debeec3139b581b99b45f241bd6f8c
Lev Butkovskiy <levbut@gmail.com>
2021-09-29 02:30:24 +0300

    Карточка автомобиля

--- src/App.js
+++ src/App.js
@@ -1,10 +1,88 @@
-import styles from './App.module.css';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import styles from './styles.module.css'
+import CarCard from './components/CarCard';
 
+const characteristics = [
+    {
+        title: 'Двигатель',
+        value: '1.6л'
+    },
+    {
+        title: 'КПП',
+        value: 'Автоматическая'
+    },
+    {
+        title: 'Цвет',
+        value: 'Серебристо-зеленый'
+    },
+];
+
+const APIUrl = 'https://api.carmart.ru/cars/temp?page=1&perPage=24&isNew%5B0%5D=1&orderBy%5B0%5D%5Bfield%5D=year&orderBy%5B0%5D%5Bdirection%5D=desc&brand%5B0%5D=Audi';
 function App() {
-  return (
-    <div className={styles.app}>
-    </div>
-  );
+    const [cars, setCars] = useState();
+
+    useEffect(() => {
+        fetch(APIUrl, {method: 'GET'})
+            .then(response => response.json())
+            .then(data => {
+                setCars(data.list)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    }, [])
+
+    const prepareStatus = (status) => {
+        if (status === 'Свободно') {
+            return 'Купить';
+        }
+        if (status === 'Заказ') {
+            return 'Купить под заказ';
+        }
+        return status
+    }
+
+    const renderCars = (car, index) => {
+        console.log(car);
+        return (
+            <CarCard
+                title={`${car.classifieds.description} ${car.feedData.equipmentName}`}
+                modelYear={car.classifieds.modelYear}
+                vin={car.vin}
+                characteristics={
+                    [
+                        {
+                            title: 'Двигатель',
+                            value: `${car.feedData.equipmentVariantEngineCapacity / 1000} л / ${car.feedData.equipmentVariantEnginePower} лс / ${car.feedData.equipmentVariantFuelType}`
+                        },
+                        {
+                            title: 'КПП / Привод',
+                            value: `${car.feedData.equipmentVariantTransmissionType} / ${car.feedData.equipmentVariantDriveType}`
+                        },
+                        {
+                            title: 'Цвет',
+                            value: car.feedData.color
+                        },
+                    ]
+                }
+                images={car.photobank.imgs}
+                price={car.legacy.price}
+                color={car.feedData.colorByClassifierName}
+                options={car.feedData.options}
+                status={prepareStatus(car.setCarStatus.status)}
+            />
+        )
+    }
+
+    return (
+        <div className={styles.app}>
+            <div className={styles.content}>
+                {cars && cars.map(renderCars)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CarCard/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function CarCard(props) {
+    const { title, modelYear, vin, images, characteristics, price, options, status } = props;
+
+    const renderCharacteristics = (item, index) => {
+        return (
+            <div className={styles.characteristic}>
+                <div className={styles.characteristicTitle}>{item.title}</div>
+                <div className={styles.characteristicValue}>{item.value}</div>
+            </div>
+        );
+    }
+
+    const renderImages = (image, index) => {
+        return (
+            <>
+            <img src={image.url} className={styles.carImage}/>
+        <img src={image.url} className={styles.carImage}/></>
+    )
+    }
+
+    const renderOptions = (option, index) => {
+        return index == 0 ? `${option.optionName}, ` : index == 1 ? `${option.optionName} ` : '';
+    }
+
+    const inclineByNumber = (number) => {
+        let lastNumber = number.toString().split('').pop();
+        switch (Number(lastNumber)) {
+            case 0: case 5: case 6: case 7: case 8: case 9: return 'пакетов';
+            case 1: return 'пакет';
+            case 2: case 3: case 4: return 'пакета';
+            default: return 'пакетов'
+        }
+    }
+
+    const addSpacesToNumber = (str) => {
+        str = str.toString();
+        return str.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, '$1' + ' ');
+    }
+
+    return (
+       <div className={styles.carCard}>
+           <div className={styles.title}>{title}<sup className={styles.modelYear}>{modelYear}</sup></div>
+           <div className={styles.vin}>{vin}</div>
+           <div className={styles.images}>
+               {images.map(renderImages)}
+           </div>
+           <div className={styles.characteristics}>
+               {characteristics.map(renderCharacteristics)}
+           </div>
+           <div className={styles.options}>
+               <div className={styles.characteristicTitle}>Пакеты</div>
+               <div className={styles.optionsList}>
+                   <div className={styles.optionsValue}>{options.map(renderOptions)}</div>
+                   <div className={styles.optionsMore}>(+еще {options.length} {inclineByNumber(options.length)})</div>
+               </div>
+           </div>
+           <div className={styles.total}>
+               <div className={styles.financial}>
+                    <div className={styles.price}>{addSpacesToNumber(price)}<span style={{color: 'black'}}> ₽</span></div>
+               </div>
+               <div className={styles.actions}>
+                   <button className={styles.buyButton} type={status === 'В поставке' || status === 'Резерв'? 'inactive' : null}>{status}</button>
+               </div>
+           </div>
+       </div>
+   )
+}
